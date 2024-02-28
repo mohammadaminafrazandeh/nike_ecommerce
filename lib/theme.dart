4a12afaf70a42e9763b641345427d35ea1117ec8
Amin <124354256+mohammadaminafrazandeh@users.noreply.github.com>
2024-02-28 16:03:04 +0330

    theme

---
+++ lib/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LightThemeColors {
+  static const primaryColor = Color(0xFF217cf3);
+  static const secondaryColor = Color(0xFF262a35);
+  static const primaryTextColor = Color(0xFF262a35);
+  static const secondaryTextColor = Color(0xFFb3b6be);
+}
